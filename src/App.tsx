36a9c4b880ef1fae410526731c255a66e3191509
charith <wijesinghecharith32@gmail.com>
2026-08-26 11:37:51 +0530

    Fix build: remove unused ScrollTitle and Cta imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import { Header } from "@/components/site/Header"
 import { Hero } from "@/components/site/Hero"
 import { Projects } from "@/components/site/Projects"
-import { ScrollTitle } from "@/components/site/ScrollTitle"
 import { Expertise } from "@/components/site/Expertise"
 import { About, Approach } from "@/components/site/AboutApproach"
 import { TechMarquee } from "@/components/site/TechMarquee"
-import { Cta } from "@/components/site/Cta"
 import { Footer } from "@/components/site/Footer"
 
 function App() {
